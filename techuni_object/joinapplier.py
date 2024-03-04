88ba3b63e51da82f2d850c70179d490e68bfb82a
KO1231 <124903312+KO1231@users.noreply.github.com>
2024-03-05 01:15:28 +0900

    [add] JoinApplierの定義に型指定を追加

--- techuni_object/joinapplier.py
+++ techuni_object/joinapplier.py
@@ -3,14 +3,14 @@ class JoinApplier:
     def __init__(self, data: dict):
         self.mail_addr = data["mail_address"]
         _applied_at = datetime.strptime(data["applied_at"], "%Y-%m-%d %H:%M:%S %z") # ex) 2024-01-01 01:11:11 +0900
-        self.applied_at = str(_applied_at)
-        self.name = data["name"]
-        self.univ = data["university"]
-        self.dep = data["department"]
-        self.grade = data["grade"]
-        self.reason = data["reason"]
-        self.opportunity = data.get("opportunity", None)  # opportunityのみNULL許容
-        self.possible_dates = data["possible_dates"]
+        self.applied_at: str = str(_applied_at)
+        self.name: str = data["name"]
+        self.univ: str = data["university"]
+        self.dep: str = data["department"]
+        self.grade: str = data["grade"]
+        self.reason: str = data["reason"]
+        self.opportunity: str | None = data.get("opportunity", None)  # opportunityのみNULL許容
+        self.possible_dates: str = data["possible_dates"]
 
     def to_dict(self):
         return {
