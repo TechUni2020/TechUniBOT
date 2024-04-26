aec119396d639671b2c5f194c8f76a661ae2aeef
KO1231 <124903312+KO1231@users.noreply.github.com>
2024-04-27 02:02:36 +0900

    [add] discord_bot の初期実装

--- techuni_discord/discord_bot.py
+++ techuni_discord/discord_bot.py
@@ -1,11 +1,23 @@
 import discord
+from discord.ext import tasks
 from techuni_object import JoinApplication
+from multiprocessing import Queue
+
+flask_applier = Queue()
+
 class TechUniDiscordBot(discord.Client):
+
     async def on_ready(self):
-        pass
+        print(f"Logged on as {self.user.name} ({self.user.id})")
 
     async def on_message(self, message):
         pass
 
-    async def onApplyForm(self, data: JoinApply):
+    async def notify_application(self, application: JoinApplication):
         pass
+
+    @tasks.loop(seconds=60)
+    async def checkForm(self):
+        while not flask_applier.empty():
+            application: JoinApplication = flask_applier.get()
+            await self.notify_application(application)
