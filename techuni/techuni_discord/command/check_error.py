bc15ea0d432f30480f878682ec259b4b86c47093
KO1231 <124903312+KO1231@users.noreply.github.com>
2024-05-07 20:05:34 +0900

    [change] OutOfTechUniGuildErrorをCommandErrorからCheckFailureに変更

--- techuni/techuni_discord/command/check_error.py
+++ techuni/techuni_discord/command/check_error.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
 
-class OutOfTechUniGuildError(commands.CommandError):
+class OutOfTechUniGuildError(commands.CheckFailure):
     def __init__(self):
         super().__init__("This command is only available in Tech.Uni Discord Server.")
