5e3ade162be1aa743a0eb47565032b3bf4788b79
KO1231 <124903312+KO1231@users.noreply.github.com>
2024-06-02 06:48:14 +0900

    [add] JoinApplicationStatusに対応するEmailTemplate情報を追加

--- techuni/techuni_object/join_application_status.py
+++ techuni/techuni_object/join_application_status.py
@@ -1,15 +1,17 @@
 import os
 from enum import Enum, auto
 from discord import ForumChannel
+from techuni.techuni_email import EmailTemplate
 
 class JoinApplicationStatus(Enum):
-    RECEIVE = auto(),
-    INVITE = auto(),
-    REJECT = auto(),
+    RECEIVE = (auto(), EmailTemplate.RECEIVE),
+    INVITE = (auto(), EmailTemplate.INVITE),
+    REJECT = (auto(), None),
 
-    def __init__(self, _id):
+    def __init__(self, _id, email_template: EmailTemplate):
         self.id = _id
         self._tag_id = None
+        self._email_template = email_template
 
     def get_tag(self, channel: ForumChannel):
         if channel is None:
@@ -21,3 +23,11 @@ class JoinApplicationStatus(Enum):
                 raise ValueError(f"Tag ID({self.name}) is not found")
 
         return channel.get_tag(self._tag_id)
+
+    def get_email_template(self):
+        if not self.need_send_email():
+            raise RuntimeError(f"Email Template is not set in {self.name}.")
+        return self._email_template
+
+    def need_send_email(self):
+        return self._email_template is not None
