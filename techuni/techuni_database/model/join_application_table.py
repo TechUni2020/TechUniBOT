105edb8d0fd1d647b571594f5937fb512d966cbc
KO1231 <124903312+KO1231@users.noreply.github.com>
2024-06-02 13:50:14 +0900

    [add] discord thread_id in JoinApplicationTable

--- techuni/techuni_database/model/join_application_table.py
+++ techuni/techuni_database/model/join_application_table.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.types import String
-from sqlalchemy.dialects.mysql import TEXT
+from sqlalchemy.dialects.mysql import TEXT, BIGINT
 
 Base = declarative_base()
 
@@ -12,11 +12,13 @@ class JoinApplicationTable(Base):
     form_id = Column(String(255), nullable=False, unique=True)
     mail_address = Column(TEXT, nullable=False)
     name = Column(TEXT, nullable=False)
+    thread_id = Column(BIGINT, nullable=False, unique=True)
 
     @classmethod
-    def from_application(cls, application):
+    def from_application(cls, application, thread_id):
         return cls(
             form_id=application.id,
             mail_address=application.mail_address,
-            name=application.name
+            name=application.name,
+            thread_id=thread_id
         )
